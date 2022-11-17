f8c8efce2e86acc0e69a18cdebebf8c512b6aa41
mbadarp <mbadarp@gmail.com>
2022-11-17 21:10:48 +0700

    template

--- application/controllers/admin/Dashboard.php
+++ application/controllers/admin/Dashboard.php
@@ -5,6 +5,10 @@ class Dashboard extends CI_Controller
 {
     public function index()
     {
-        $this->load->view('admin/dashboard/index');
+        $data = [
+            'title' => 'Dashboard',
+            'content' => 'admin/dashboard/index'
+        ];
+        $this->load->view('admin/templates/main', $data);
     }
 }
